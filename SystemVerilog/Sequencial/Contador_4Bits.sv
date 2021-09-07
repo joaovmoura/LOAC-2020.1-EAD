15b5e78fcf33f7d0f1a47692b682ab2850833426
joaovmoura <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2021-09-07 16:56:50 -0300

    Contdor ok

--- SystemVerilog/Sequencial/Contador_4Bits.sv
+++ SystemVerilog/Sequencial/Contador_4Bits.sv
@@ -1,8 +1,7 @@
-// João Vitor Moura Figueiredo
-// Contador 4 bits - contagem com reset, incremento de 1/3 , crescente/decrescente, congelamento e saturação
+// João Vitor Moura Figueiredo	
+// Contador 4 bits - reset, saturação, congelamento, incrimento de 3
 // DESCRIPTION: Verilator: Systemverilog example module
 // with interface to switch buttons, LEDs, LCD and register display
-
 
 parameter divide_by=100000000;  // divisor do clock de referência
 // A frequencia do clock de referencia é 50 MHz.
@@ -41,14 +40,13 @@ module top(input  logic clk_2,
        else                   lcd_registrador[i] <= ~SWI;
   end
 
-	parameter NBITS = 4;
+		parameter NBITS_COUNT = 4;
   	parameter INCR_1 = 1;
   	parameter INCR_3 = 3;
   	parameter INICIO_CONT = 0;
   	parameter FINAL_CONT = 15;
   	logic reset, decrescente, incr_3, congelamento, saturacao;
-  	logic [NBITS-1:0] counter;
-    
+   
     //Atribuindo chaves de entrada e inicializando o contador
 	always_comb	begin
 		reset <= SWI[0];
@@ -56,27 +54,27 @@ module top(input  logic clk_2,
 		incr_3 <= SWI[2];
 		congelamento <= SWI[3];
 		saturacao <= SWI[4];
-		counter = INICIO_CONT;
 	end
-	
+	  
+		logic [NBITS_COUNT-1:0] counter;
     //Calculo do contador conforme o pulso do clock, considerando as alterações nas entradas
 	always_ff@(posedge clk_2) begin
 		if(~congelamento) begin
-			if(saturacao && (counter == FINAL_CONT || counter == INICIO_CONT)) counter = counter;
+			if(saturacao && (counter == FINAL_CONT || counter == INICIO_CONT)) counter <= counter;
             else begin
-				if(reset) counter = INICIO_CONT;
+				if(reset) counter <= INICIO_CONT;
 				else if(decrescente) begin
-					if(incr_3) counter = counter - INCR_3;
-					else counter =  counter - INCR_1;
+					if(incr_3) counter <= counter - INCR_3;
+					else counter <=  counter - INCR_1;
 				end
 				else begin 
-					if(incr_3) counter = counter + INCR_3;
-					else counter =  counter + INCR_1;			
+					if(incr_3) counter <= counter + INCR_3;
+					else counter <=  counter + INCR_1;			
 				end	
 			end
 		end
 		else begin
-			counter = counter;
+			counter <= counter;
 		end
 	end
     
@@ -84,6 +82,5 @@ module top(input  logic clk_2,
 	always_comb begin
 		lcd_b = counter;
 	end
+
 endmodule
-
-
